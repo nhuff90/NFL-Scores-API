5662a4491665733c19c32c6701b4e439953c2278
Nate Huff <nhuff90@gmail.com>
2019-09-04 15:53:17 -0400

    Initial Commit

---
+++ src/main/java/com/nhuff/NFL/Scores/API/NflScoresApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhuff.NFL.Scores.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflScoresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflScoresApiApplication.class, args);
+	}
+
+}
